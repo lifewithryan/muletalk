9ff3b73375f532ae0767c4518bafffa159969ff8
Ryan Headley <rheadley@flexion.us>
2012-09-25 09:49:39 -0500

    adding specific functionality for muletalk (see details)
    adding ability to create a room from the interface.
    creating one default room called 'bedford'.
    adding new style and moving it to separate style.css file
    fixed bug where chat wasn't scrolling to most recent entry
    adding the userlist back to the left hand side, will show all users
    regardless of room however.

--- app.js
+++ app.js
@@ -7,40 +7,47 @@ app.listen(8080);
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
+app.get('/style.css', function (req, res) {
+  res.sendfile(__dirname + '/style.css');
+});
 
 // usernames which are currently connected to the chat
 var usernames = {};
 
 // rooms which are currently available in chat
-var rooms = ['room1','room2','room3'];
+var rooms = ['bedford'];
 
 io.sockets.on('connection', function (socket) {
-	
+
 	// when the client emits 'adduser', this listens and executes
 	socket.on('adduser', function(username){
 		// store the username in the socket session for this client
 		socket.username = username;
 		// store the room name in the socket session for this client
-		socket.room = 'room1';
+		socket.room = 'bedford';
 		// add the client's username to the global list
 		usernames[username] = username;
 		// send client to room 1
-		socket.join('room1');
+		socket.join('bedford');
 		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
+		socket.emit('updatechat', 'SERVER', 'you have connected to ' + socket.room);
 		// echo to room 1 that a person has connected to their room
-		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
-		socket.emit('updaterooms', rooms, 'room1');
+		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', username + ' has connected to this room');
+		socket.emit('updaterooms', rooms, 'bedford');
+		socket.emit('updateusers', usernames);
+		socket.broadcast.emit('updateusers', usernames);
 	});
-	
+
 	// when the client emits 'sendchat', this listens and executes
 	socket.on('sendchat', function (data) {
 		// we tell the client to execute 'updatechat' with 2 parameters
 		io.sockets.in(socket.room).emit('updatechat', socket.username, data);
 	});
-	
+
 	socket.on('switchRoom', function(newroom){
+		// leave the current room (stored in session)
 		socket.leave(socket.room);
+		// join new room, received as function parameter
 		socket.join(newroom);
 		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
 		// sent message to OLD room
@@ -50,7 +57,21 @@ io.sockets.on('connection', function (socket) {
 		socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
 		socket.emit('updaterooms', rooms, newroom);
 	});
-	
+
+	// user creates room
+	socket.on('createRoom', function(roomName) {
+		rooms.push(roomName);
+		socket.emit('addedRoom', rooms, roomName);
+		socket.broadcast.emit('addedRoom', rooms, roomName);
+	});
+
+	/*socket.on('command', function(commandString) {
+		cmd = commandString.split(' ');
+
+		if(cmd[0] == "deleteroom") {
+			rooms
+		} else if(commandString == )
+	});*/
 
 	// when the user disconnects.. perform this
 	socket.on('disconnect', function(){
@@ -62,4 +83,6 @@ io.sockets.on('connection', function (socket) {
 		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
 		socket.leave(socket.room);
 	});
+
+
 });
